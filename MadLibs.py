c8e4fcd5c606e2404fe2b1b25250217b2b2bdbce
michaelbspur <55725624+michaelbspur@users.noreply.github.com>
2021-05-23 16:12:29 -0600

    Add files via upload

---
+++ MadLibs.py
@@ -0,0 +1,19 @@
+Name = input("Enter a first name: ")
+Name2 = input("Enter a last name: ")
+verb1 = input("Enter a verb: ")
+occupation = input("Enter an occupation: ")
+occupation2 = input("Enter another occupation: ")
+verb2 = input("Enter a verb: ")
+sound = input("Give a sound: ")
+adjective = input("Enter an adjective: ")
+
+print(Name + " " + Name2 + " loves " + verb1 + ".")
+print(Name + " works as a " + occupation + " but he would rather be a " + occupation2 + ".")
+print(Name + " was lost one day and wanted to " + verb2 + " but then heard a " + sound)
+print(Name + " was terrified and decided to head home after a " + adjective + " day.")
+
+
+
+
+
+
